686535c4eed78116dbceabd972e9ed74f8a713c4
Bartholomäus Dedersen <bartek@selbstapotheose.de>
2019-10-11 16:28:07 +0200

    azure queues and config

--- watchdog.sh
+++ watchdog.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#CONFIG PARAMETERS
+if [ ! -f "./config" ] && [ -f "./config.demo" ] ; then
+    echo "Please set own configuration from config.demo"
+    exit
+fi
+
+source ./config
+
 debug=true
 
 function debugLog() {
@@ -25,7 +33,7 @@ function isPrimaryAPAvailable() {
 
 function isBackupAPAvailable() {
     debugCaller
-    return 1
+    return 0
     #return codes:
     #0=ok
     #1=no internet
@@ -44,14 +52,29 @@ function changeNetworkToPrimaryAP() {
 
 function sendDowntimeRestoredMessage() {
     debugCaller
-    return
+    sendQueueMessage "downtime Restored"
 }
 
 function sendDowntimeHappeningMessage() {
     debugCaller
+    sendQueueMessage "downtime Happening"
     return
 }
 
+# parameter: log message
+function sendQueueMessage() {
+    debugCaller
+    DATE_NOW=$(date -Ru | sed 's/\+0000/GMT/')
+    AZ_VERSION="2018-03-28"
+    AZ_STORAGE_URL="https://${AZ_STORAGE_NAME}.queue.core.windows.net"
+    AZ_STORAGE_TARGET="${AZ_STORAGE_URL}/${AZ_STORAGE_QUEUE}/"
+    LOG_MSG=$1
+
+    curl -v -X POST -H "Conent-Type:application/json" -H "x-ms-date: ${DATE_NOW}" -H "x-ms-version: ${AZ_VERSION}" "${AZ_STORAGE_TARGET}messages?visibilitytimeout=30&timeout=30${AZ_SAS_TOKEN}" -d "${LOG_MSG}"
+    return
+}
+
+#init state variables
 wasDown=false
 
 #do forever
