ab88b60fc25d2433ab8cb3f3dcbce310f39e5411
Bartholomäus Dedersen <bartek@selbstapotheose.de>
2019-10-11 15:57:34 +0200

    syntax fix and debug

--- watchdog.sh
+++ watchdog.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
+debug=true
+
+function debugLog() {
+    if [ "$debug" = true ] ; then
+        echo $1
+    fi
+}
+
+function debugCaller() {
+    if [ "$debug" = true ] ; then
+        echo Func call: ${FUNCNAME[1]}
+    fi
+}
+
 function isPrimaryAPAvailable() {
-    return 0;
+    debugCaller
+    return 1
     #return codes:
     #0=ok
     #1=no internet
-    #2=no router
+    #2=no wifi ap
 }
 
 function isBackupAPAvailable() {
-    return 0
+    debugCaller
+    return 1
     #return codes:
     #0=ok
     #1=no internet
-    #2=no router
+    #2=no wifi ap
 }
 
 function changeNetworkToBackupAP() {
+    debugCaller
     return
 }
 
 function changeNetworkToPrimaryAP() {
+    debugCaller
     return
 }
 
 function sendDowntimeRestoredMessage() {
+    debugCaller
     return
 }
 
 function sendDowntimeHappeningMessage() {
+    debugCaller
     return
 }
 
@@ -41,27 +61,24 @@ do
     primaryAPAvailable=$?
 
     # reset after successful reconnect after downtime
-    if [ $primaryAPAvailable -eq 0 ] && [ wasDown = true ]
-        then
-            sendDowntimeRestoreMessage
-            wasDown=false
+    if [ $primaryAPAvailable -eq 0 ] && [ wasDown = true ] ; then
+        sendDowntimeRestoreMessage
+        wasDown=false
     fi
     
 
-    if [ $primaryAPAvailable -ne 0 ]
-        then
-            changeNetworkToBackupAP
-            isBackupAPAvailable
-            backupAPAvailable=$?
+    if [ $primaryAPAvailable -ne 0 ] ; then
+        changeNetworkToBackupAP
+        isBackupAPAvailable
+        backupAPAvailable=$?
 
-            if [ backupAPAvailable -eq 0 ]
-                then
-                    sendDowntimeHappeningMessage
-            fi
+        if [ $backupAPAvailable -eq 0 ] ; then
+            sendDowntimeHappeningMessage
+        fi
         
-            wasDown=true
+        wasDown=true
 
-            changeNetworkToPrimaryAP
+        changeNetworkToPrimaryAP
     fi
 
     sleep 1m
